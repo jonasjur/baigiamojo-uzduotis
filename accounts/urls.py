aa50b9b7ec36e807d9ff829165a917a06abc2c89
jonasjur <jonasjurkevicius@yahoo.com>
2023-09-07 23:10:56 +0300

    prisijungimas

---
+++ accounts/urls.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.indexView,name="home"),
+    path('dashboard/',views.dashboardView,name="dashboard"),
+    path('login/',LoginView.as_view(),name="login_url"),
+    path('register/',views.registerView,name="register_url"),
+    path('logout/',LogoutView.as_view(next_page='dashboard'),name="logout"),
+
+]
---
+++ svetaine/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import path, include
+
+from accounts import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
+    path('',views.indexView,name="home"),
+]
