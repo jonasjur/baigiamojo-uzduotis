1df7ec2830290771643668de49718ff30ae88c72
jonasjur <jonasjurkevicius@yahoo.com>
2023-09-09 07:17:21 +0300

    galima atsakyti i klausimus

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,8 @@ from django import forms
 from django.db import models
 from django.contrib.auth.models import User
 
+
+
 class NeatsakytasKlausimas(models.Model):
     vartotojas = models.ForeignKey(User, on_delete=models.CASCADE)
     klausimas = models.TextField()
@@ -13,3 +15,13 @@ class KlausimoForma(forms.ModelForm):
     class Meta:
         model = NeatsakytasKlausimas
         fields = ['klausimas']
+
+
+class AtsakytasKlausimas(models.Model):
+    vartotojas = models.ForeignKey(User, on_delete=models.CASCADE)
+    klausimas = models.TextField()
+    atsakymas = models.TextField()  # Add a field for answers
+    sukurtas = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.klausimas
--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,32 @@
-from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-
-from accounts.models import KlausimoForma, NeatsakytasKlausimas
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from accounts.models import KlausimoForma, NeatsakytasKlausimas, AtsakytasKlausimas
 
 
 def indexView(request):
-    return render(request,'index.html')
+    return render(request, 'index.html')
 
-@login_required()
+
+@login_required
 def dashboardView(request):
-    return render (request, 'dashboard.html')
+    neatsakyti_klausimai = NeatsakytasKlausimas.objects.filter(vartotojas=request.user)
+    atsakyti_klausimai = AtsakytasKlausimas.objects.filter(vartotojas=request.user)
+    forma = KlausimoForma()
+
+    if request.method == "POST":
+        forma = KlausimoForma(request.POST)
+        if forma.is_valid():
+            klausimas = forma.save(commit=False)
+            klausimas.vartotojas = request.user
+            klausimas.save()
+            return redirect('dashboard')
+
+    return render(request, 'dashboard.html',
+                  {'neatsakyti_klausimai': neatsakyti_klausimai, 'atsakyti_klausimai': atsakyti_klausimai,
+                   'forma': forma})
+
 
 @login_required
 def klausimoIvedimoVaizdas(request):
@@ -23,25 +39,9 @@ def klausimoIvedimoVaizdas(request):
             return redirect('dashboard')
     else:
         forma = KlausimoForma()
+
     return render(request, 'klausimo_ivedimo_forma.html', {'forma': forma})
 
-
-@login_required
-def dashboardView(request):
-
-    neatsakyti_klausimai = NeatsakytasKlausimas.objects.filter(vartotojas=request.user)
-
-    if request.method == "POST":
-        forma = KlausimoForma(request.POST)
-        if forma.is_valid():
-            klausimas = forma.save(commit=False)
-            klausimas.vartotojas = request.user
-            klausimas.save()
-            return redirect('dashboard')  # Redirect back to the dashboard after creating a question
-    else:
-        forma = KlausimoForma()
-
-    return render(request, 'dashboard.html', {'neatsakyti_klausimai': neatsakyti_klausimai, 'forma': forma})
 
 def registerView(request):
     if request.method == "POST":
@@ -51,9 +51,27 @@ def registerView(request):
             return redirect('login_url')
     else:
         form = UserCreationForm()
-    return render (request, 'registration/register.html',{'form':form})
+    return render(request, 'registration/register.html', {'form': form})
 
+
+@login_required
 def atvaizduotiKlausima(request, klausimo_id):
     klausimas = NeatsakytasKlausimas.objects.get(id=klausimo_id)
+
+
+    if request.method == "POST":
+        atsakymas = request.POST.get('atsakymas')
+        if atsakymas:
+
+            AtsakytasKlausimas.objects.create(
+                vartotojas=request.user,
+                klausimas=klausimas.klausimas,
+                atsakymas=atsakymas,
+            )
+
+            klausimas.delete()
+            return redirect('dashboard')
+
     return render(request, 'klausimo_atvaizdavimas.html', {'klausimas': klausimas})
+
 
