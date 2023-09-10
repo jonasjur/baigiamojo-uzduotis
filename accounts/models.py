fd0b533948ee7261a550d00b98aec3b4de3d34d2
jonasjur <jonasjurkevicius@yahoo.com>
2023-09-10 17:59:05 +0300

    rodo kuris vartotojas kuri klausima sukure ir atsake

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,9 @@
-from django import forms
+
 from django.db import models
 from django.contrib.auth.models import User
 
 
-
+# Apibrėžiamas Django modelis neatsakytų klausimų saugojimui.
 class NeatsakytasKlausimas(models.Model):
     vartotojas = models.ForeignKey(User, on_delete=models.CASCADE)
     klausimas = models.TextField()
@@ -12,12 +12,13 @@ class NeatsakytasKlausimas(models.Model):
     def __str__(self):
         return self.klausimas
 
-
+# Apibrėžiamas Django modelis atsakytų klausimų saugojimui.
 
 class AtsakytasKlausimas(models.Model):
     vartotojas = models.ForeignKey(User, on_delete=models.CASCADE)
     klausimas = models.TextField()
     atsakymas = models.TextField()
+    atsakymo_vartotojas = models.ForeignKey(User, related_name='answers', on_delete=models.CASCADE)  # Add this field
     sukurtas = models.DateTimeField(auto_now_add=True)
     liked_by = models.ManyToManyField(User, related_name='liked_answers', blank=True)
 
